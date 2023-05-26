5e5bd264908c3aec3b3d2236ce95a3e3802b13bc
sam.huelsdonk@gmail.com <sam.huelsdonk@gmail.com>
2023-05-26 16:42:47 -0500

    postman update

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -25,14 +25,14 @@ export const updateDisplayNames = functions.firestore
     .document("Users/{uid}").onWrite(async (change, context) => GameAPI.updateUserMetaData(change, context));
 
 gameAPIApp.post("/games/create", async (req, res) => GameAPI.create(req, res));
+gameAPIApp.post("/games/delete", async (req, res) => GameAPI.delete(req, res));
 gameAPIApp.post("/games/join", async (req, res) => GameAPI.join(req, res));
 gameAPIApp.post("/games/leave", async (req, res) => GameAPI.leave(req, res));
-gameAPIApp.post("/games/sit", async (req, res) => GameAPI.sit(req, res));
-gameAPIApp.post("/games/stand", async (req, res) => GameAPI.stand(req, res));
-gameAPIApp.post("/games/delete", async (req, res) => GameAPI.delete(req, res));
-gameAPIApp.post("/games/options", async (req, res) => GameAPI.options(req, res));
 gameAPIApp.post("/games/message", async (req, res) => GameAPI.message(req, res));
 gameAPIApp.post("/games/message/delete", async (req, res) => GameAPI.messageDelete(req, res));
+gameAPIApp.post("/games/options", async (req, res) => GameAPI.options(req, res));
+gameAPIApp.post("/games/sit", async (req, res) => GameAPI.sit(req, res));
+gameAPIApp.post("/games/stand", async (req, res) => GameAPI.stand(req, res));
 
 gameAPIApp.post("/guess/action", async (req, res) => GuessAPI.userAction(req, res));
 gameAPIApp.post("/match/action", async (req, res) => MatchAPI.userAction(req, res));
